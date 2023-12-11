ff52ad7fc536bb82e35c43139265eff901182302
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-12-11 22:28:56 +0200

    Feat(JS) Added code to quote

--- src/js/02-quote.js
+++ src/js/02-quote.js
@@ -1,1 +1,23 @@
+import APIService from './00-api';
 
+const quoteOfTheDay = document.querySelector('.js-quote');
+
+const apiService = new APIService();
+
+quote();
+
+async function quote() {
+  try {
+    const data = await apiService.getQuote();
+    displayQuote(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayQuote({ author, quote }) {
+  quoteOfTheDay.innerHTML = `
+  <p class="text__quote">${quote}</p>
+  <h4 class="text__author">${author}</h4>
+    `;
+}
