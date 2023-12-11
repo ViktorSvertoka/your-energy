0329f4cd31ced65894639cbc142c54d397cf2b9e
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-12-11 21:18:56 +0200

    add header

--- src/js/01-mobile-menu.js
+++ src/js/01-mobile-menu.js
@@ -48,3 +48,50 @@
 //     }
 //   });
 // });
+
+const logo = document.getElementById('logo');
+const toggleButtons = document.getElementById('toggleButtons');
+const burgerMenu = document.getElementById('burgerMenu');
+const socialMedia = document.getElementById('socialMedia');
+const openHomeButton = document.getElementById('homeButton');
+const openFavoritesButton = document.getElementById('favoritesButton');
+
+function openMobileMenu() {}
+
+function closeMobileMenu() {}
+
+function toggleButtonsFunction() {
+  const homeButton = document.getElementById('homeButton');
+  const favoritesButton = document.getElementById('favoritesButton');
+
+  if (!homeButton.classList.contains('active')) {
+    homeButton.classList.add('active');
+    favoritesButton.classList.remove('active');
+    openMobileMenu();
+    redirectToHome();
+  } else {
+    homeButton.classList.remove('active');
+    favoritesButton.classList.add('active');
+    openMobileMenu();
+    redirectToFavorites();
+  }
+}
+
+function redirectToHome() {
+  //   window.location.href = '../partials/02-hero.html';
+}
+
+function redirectToFavorites() {
+  //   window.location.href = '../partials/10-favorites.html';
+}
+
+toggleButtons.addEventListener('click', toggleButtonsFunction);
+// burgerMenu.addEventListener('click');
+openHomeButton.addEventListener('click', () => {
+  openMobileMenu();
+  redirectToHome();
+});
+openFavoritesButton.addEventListener('click', () => {
+  openMobileMenu();
+  redirectToFavorites();
+});
