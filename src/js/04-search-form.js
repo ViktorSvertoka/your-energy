b961a53e1a3406b1e3c05ae9de57039c56908773
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-12-14 00:28:28 +0200

    Fix(JS) Update code

--- src/js/04-search-form.js
+++ src/js/04-search-form.js
@@ -1,43 +1,43 @@
-import APIService from './00-api';
-const apiService = new APIService();
-const listItem = document.querySelector('.js-list');
+// import APIService from './00-api';
+// const apiService = new APIService();
+// const listItem = document.querySelector('.js-list');
 
-listItem.addEventListener('click', onCardClick);
+// listItem.addEventListener('click', onCardClick);
 
-async function onCardClick(e) {
-  if (!e.target.closest('.filters__item')) {
-    return;
-  }
+// async function onCardClick(e) {
+//   if (!e.target.closest('.filters__item')) {
+//     return;
+//   }
 
-  const item = e.target.closest('.filters__item');
+//   const item = e.target.closest('.filters__item');
 
-  const filter = item.children[0].innerText.toLowerCase();
-  const name = item.children[1].innerText.toLowerCase();
+//   const filter = item.children[0].innerText.toLowerCase();
+//   const name = item.children[1].innerText.toLowerCase();
 
-  try {
-    const data = await apiService.getExercises(filter, name);
-    renderExercises(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+//   try {
+//     const data = await apiService.getExercises(filter, name);
+//     renderExercises(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-function renderExercises(data) {
-  listItem.innerHTML = '';
-  const markup = data
-    .map(({ name, burnedCalories, bodyPart, target }) => {
-      return `
-  <li class="filters__item">
-    <p class="filters__pre-title-second"></p>
-    <button class="filters__btn-second" type="button">svg</button>
-    <h2 class="filters__title-second">${name}</h2>
-    <p class="filters__text-second"><span>Burned calories:</span>${burnedCalories}</p>
-    <p class="filters__text-second"><span>Body part:</span>${bodyPart}</p>
-    <p class="filters__text-second"><span>Target:</span>${target}</p>
-  </li>
-    `;
-    })
-    .join('');
+// function renderExercises(data) {
+//   listItem.innerHTML = '';
+//   const markup = data
+//     .map(({ name, burnedCalories, bodyPart, target }) => {
+//       return `
+//   <li class="filters__item">
+//     <p class="filters__pre-title-second"></p>
+//     <button class="filters__btn-second" type="button">svg</button>
+//     <h2 class="filters__title-second">${name}</h2>
+//     <p class="filters__text-second"><span>Burned calories:</span>${burnedCalories}</p>
+//     <p class="filters__text-second"><span>Body part:</span>${bodyPart}</p>
+//     <p class="filters__text-second"><span>Target:</span>${target}</p>
+//   </li>
+//     `;
+//     })
+//     .join('');
 
-  listItem.insertAdjacentHTML('beforeend', markup);
-}
+//   listItem.insertAdjacentHTML('beforeend', markup);
+// }
--- src/js/05-modal-exercises.js
+++ src/js/05-modal-exercises.js
@@ -3,143 +3,131 @@ const apiService = new APIService();
 
 const modalExercises = document.querySelector('.modal-exercises');
 const overlay = document.querySelector('.overlay');
-const modalOpen = document.querySelector('.filters__btn-second');
 const listItem = document.querySelector('.js-list');
 
 listItem.addEventListener('click', onExercisesCardClick);
-
-// modalOpen.addEventListener('click', onExercisesCardClick);
-
-// addModalListener(exercisesList);
-
-// function addModalListener(listRef) {
-//   if (!listRef) {
-//     return;
-//   }
-//   listRef.addEventListener('click', onExercisesCardClick);
 
 async function onExercisesCardClick(event) {
   if (!event.target.closest('.filters__btn-second')) {
     return;
   }
 
-  //   try {
-  const exerciseID = event.target
-    .closest('.filters__btn-second')
-    .getAttribute('data-id');
+  try {
+    const exerciseID = event.target
+      .closest('.filters__btn-second')
+      .getAttribute('data-id');
 
-  console.log(exerciseID);
-  //     const exerciseData = await apiService.getExercisesById(exerciseID);
-  //     const markup = createMarkup(exerciseData);
-  //     updateModal(markup);
-  //     btnModalClose.addEventListener('click', closeModalExercises);
-  //     openModalExercises();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
+    const exerciseData = await apiService.getExercisesById(exerciseID);
+
+    const markup = createMarkup(exerciseData);
+    updateModal(markup);
+
+    openModalExercises();
+
+    console.log(modalExercises);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// function openModalExercises() {
-//   const lockPaddingValue = window.innerWidth - document.body.offsetWidth + 'px';
+function openModalExercises() {
+  const lockPaddingValue = window.innerWidth - document.body.offsetWidth + 'px';
 
-//   modalExercises.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-//   document.body.style.paddingRight = lockPaddingValue;
-//   document.body.style.overflow = 'hidden';
-// }
+  modalExercises.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  document.body.style.paddingRight = lockPaddingValue;
+  document.body.style.overflow = 'hidden';
+}
 
-// function updateModal(markup) {
-//   modalExercises.innerHTML = markup;
-// }
-// // // }
+function updateModal(markup) {
+  modalExercises.innerHTML = markup;
+}
 
-// // // Треба вставити .map і .join('') але поки що не розумію як і куди. Також у нас гівка приходить різного розміру треба зробити srcset
-// function createMarkup({
-//   _id,
-//   bodyPart,
-//   equipment,
-//   gifUrl,
-//   name,
-//   target,
-//   description,
-//   rating,
-//   burnedCalories,
-//   time,
-//   popularity,
-// }) {
-//   const getExerciseGif = getGif(gifUrl);
-//   function getGif(gifUrl) {
-//     if (gifUrl === null || !gifUrl) {
-//       return;
-//       // Треба заглушку
-//     }
-//     return `src="${gifUrl}"`;
-//   }
-//   return `
-//   <div class="modal-exercises__container" data-id="${_id}">
-//   <button class="modal-exercises__btn-close"><svg width="24" height="24">
-//     <use href="./img/sprite.svg#icon-menu-mobile-close"></use>
-//   </svg>
-//   </button>
-//   <img
-//     class="modal-exercises__img"
-//     ${getExerciseGif}
-//     alt="Exercise video"
-//     loading="lazy"
-//   />
-//   <div class="modal-exercises__card">
-//     <h2 class="modal-exercises__name">${name}</h2>
-//     <div class="modal-exercises__block">
-//       <!-- Зірочки -->
-//       <ul class="modal-exercises__list">
-//         <li class="modal-exercises__item">
-//           <h3 class="modal-exercises__subtitle">Target</h3>
-//           <p class="modal-exercises__text">${target}</p>
-//         </li>
-//         <li class="modal-exercises__item">
-//           <h3 class="modal-exercises__subtitle">Body Part</h3>
-//           <p class="modal-exercises__text">${bodyPart}</p>
-//         </li>
-//         <li class="modal-exercises__item">
-//           <h3 class="modal-exercises__subtitle">Equipment</h3>
-//           <p class="modal-exercises__text">${equipment}</p>
-//         </li>
-//         <li class="modal-exercises__item">
-//           <h3 class="modal-exercises__subtitle">Popular</h3>
-//           <p class="modal-exercises__text">${popularity}</p>
-//         </li>
-//         <li class="modal-exercises__item">
-//           <h3 class="modal-exercises__subtitle">Burned calories</h3>
-//           <p class="modal-exercises__text">${burnedCalories}/${time}</p>
-//         </li>
-//       </ul>
-//       <p class="modal-exercises__description">${description}</p>
-//     </div>
-//     <div class="modal-exercises__btn-container">
-//         <button class="modal-exercises__btn-favorites">
-//             Add to favorites
-//             <svg class="btn-favorites__icon" width="18" height="18">
-//               <use href="../img/sprite.svg#icon-favorites"></use>
-//             </svg>
-//           </button>
-//           <button class="modal-exercises__btn-rating">Give a rating</button>
-//     </div>
-//   </div>
-// </div>
-// `;
-// }
+function createMarkup({
+  _id,
+  bodyPart,
+  equipment,
+  gifUrl,
+  name,
+  target,
+  description,
+  rating,
+  burnedCalories,
+  time,
+  popularity,
+}) {
+  const getExerciseGif = getGif(gifUrl);
+  function getGif(gifUrl) {
+    if (gifUrl === null || !gifUrl) {
+      return;
+      // Треба заглушку
+    }
+    return `src="${gifUrl}"`;
+  }
+  return `
+  <div class="modal-exercises__container" data-id="${_id}">
+  <button class="modal-exercises__btn-close"><svg width="24" height="24">
+    <use href="./img/sprite.svg#icon-menu-mobile-close"></use>
+  </svg>
+  </button>
+  <img
+    class="modal-exercises__img"
+    ${getExerciseGif}
+    alt="Exercise video"
+    loading="lazy"
+  />
+  <div class="modal-exercises__card">
+    <h2 class="modal-exercises__name">${name}</h2>
+    <div class="modal-exercises__block">
+      <!-- Зірочки -->
+      <ul class="modal-exercises__list">
+        <li class="modal-exercises__item">
+          <h3 class="modal-exercises__subtitle">Target</h3>
+          <p class="modal-exercises__text">${target}</p>
+        </li>
+        <li class="modal-exercises__item">
+          <h3 class="modal-exercises__subtitle">Body Part</h3>
+          <p class="modal-exercises__text">${bodyPart}</p>
+        </li>
+        <li class="modal-exercises__item">
+          <h3 class="modal-exercises__subtitle">Equipment</h3>
+          <p class="modal-exercises__text">${equipment}</p>
+        </li>
+        <li class="modal-exercises__item">
+          <h3 class="modal-exercises__subtitle">Popular</h3>
+          <p class="modal-exercises__text">${popularity}</p>
+        </li>
+        <li class="modal-exercises__item">
+          <h3 class="modal-exercises__subtitle">Burned calories</h3>
+          <p class="modal-exercises__text">${burnedCalories}/${time}</p>
+        </li>
+      </ul>
+      <p class="modal-exercises__description">${description}</p>
+    </div>
+    <div class="modal-exercises__btn-container">
+        <button class="modal-exercises__btn-favorites">
+            Add to favorites
+            <svg class="btn-favorites__icon" width="18" height="18">
+              <use href="../img/sprite.svg#icon-favorites"></use>
+            </svg>
+          </button>
+          <button class="modal-exercises__btn-rating">Give a rating</button>
+    </div>
+  </div>
+</div>
+`;
+}
 
-// function closeModalExercises() {
-//   modalExercises.classList.add('hidden');
-//   overlay.classList.add('hidden');
-//   document.body.style.paddingRight = '0px';
-//   document.body.style.overflow = 'auto';
-//   checkStorage();
-// }
+function closeModalExercises() {
+  modalExercises.classList.add('hidden');
+  overlay.classList.add('hidden');
+  document.body.style.paddingRight = '0px';
+  document.body.style.overflow = 'auto';
+}
 
-// overlay.addEventListener('click', closeModalExercises);
-// window.addEventListener('keydown', function (event) {
-//   if (event.key === 'Escape' && !modalExercises.classList.contains('hidden')) {
-//     closeModalExercises();
-//   }
-// });
+overlay.addEventListener('click', closeModalExercises);
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modalExercises.classList.contains('hidden')) {
+    closeModalExercises();
+  }
+});
