8596673a00ba345a89db7ac2a7bf0251ff722e6d
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-12-14 00:29:39 +0200

    update header

--- src/js/01-mobile-menu.js
+++ src/js/01-mobile-menu.js
@@ -1,89 +1,53 @@
-// const logo = document.getElementById('logo');
-// const toggleButtons = document.getElementById('toggleButtons');
-// const burgerMenu = document.getElementById('burgerMenu');
-// const socialMedia = document.getElementById('socialMedia');
-// const openHomeButton = document.getElementById('homeButton');
-// const openFavoritesButton = document.getElementById('favoritesButton');
-// const modal = document.getElementById('myModal');
-// const closeModal = document.getElementById('closeModal');
-// const modalContent = document.getElementById('modalContent');
+const burgerMenu = document.getElementById('burgerMenu');
+const modalOverlay = document.getElementById('modalOverlay');
+const closeModalButton = document.getElementById('closeModal');
+const openHomeButton = document.getElementById('homeButton');
+const openFavoritesButton = document.getElementById('favoritesButton');
 
-// function openMobileMenu() {
-//   showModal();
-// }
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
-// function closeMobileMenu() {
-//   closeModal();
-// }
+closeModalButton.addEventListener('click', closeModal);
 
-// function toggleButtonsFunction() {
-//   if (!openHomeButton.classList.contains('active')) {
-//     openHomeButton.classList.add('active');
-//     openFavoritesButton.classList.remove('active');
-//     openMobileMenu();
-//     redirectToHome();
-//   } else {
-//     openHomeButton.classList.remove('active');
-//     openFavoritesButton.classList.add('active');
-//     openMobileMenu();
-//     redirectToFavorites();
-//   }
-// }
+openHomeButton.addEventListener('click', () => {
+  toggleButtonsFunction();
+  redirectToHome();
+});
 
-// function redirectToHome() {
-//   window.location.href = '../partials/02-hero.html';
-// }
+openFavoritesButton.addEventListener('click', () => {
+  toggleButtonsFunction();
+  redirectToFavorites();
+});
 
-// function redirectToFavorites() {
-//   window.location.href = '../partials/10-favorites.html';
-// }
+function toggleMobileMenu() {
+  if (modalOverlay.style.display === 'block') {
+    closeModal();
+  } else {
+    openModal();
+  }
+}
 
-// function showModal() {
-//   modalContent.innerHTML = '<p>Content for Home or Favorite goes here.</p>';
-//   modal.style.display = 'block';
-// }
+function openModal() {
+  modalOverlay.style.display = 'block';
+}
 
-// function closeModal() {
-//   modal.style.display = 'none';
-// }
+function closeModal() {
+  modalOverlay.style.display = 'none';
+}
 
-// toggleButtons.addEventListener('click', toggleButtonsFunction);
-// burgerMenu.addEventListener('click', openMobileMenu);
-// closeModal.addEventListener('click', closeMobileMenu);
+function toggleButtonsFunction() {
+  if (!openHomeButton.classList.contains('active')) {
+    openHomeButton.classList.add('active');
+    openFavoritesButton.classList.remove('active');
+  } else {
+    openHomeButton.classList.remove('active');
+    openFavoritesButton.classList.add('active');
+  }
+}
 
-// openHomeButton.addEventListener('click', () => {
-//   openMobileMenu();
-//   redirectToHome();
-// });
+function redirectToHome() {
+  window.location.href = '../partials/10-favorites.html';
+}
 
-// openFavoritesButton.addEventListener('click', () => {
-//   openMobileMenu();
-//   redirectToFavorites();
-// });
-
-// toggleButtons.addEventListener('click', function () {
-//   console.log('Toggle buttons clicked');
-//   toggleButtonsFunction();
-// });
-
-// burgerMenu.addEventListener('click', function () {
-//   console.log('Burger menu clicked');
-//   openMobileMenu();
-// });
-
-// closeModal.addEventListener('click', function () {
-//   console.log('Close modal clicked');
-//   closeMobileMenu();
-// });
-
-// openHomeButton.addEventListener('click', function () {
-//   console.log('Open home button clicked');
-//   openMobileMenu();
-//   redirectToHome();
-// });
-
-// openFavoritesButton.addEventListener('click', function () {
-//   console.log('Open favorites button clicked');
-//   openMobileMenu();
-//   redirectToFavorites();
-// });
+function redirectToFavorites() {
+  window.location.href = '../partials/10-favorites.html';
+}
