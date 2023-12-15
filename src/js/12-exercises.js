b8b82319755760f3fa923945d0ce119cf932cbf3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-12-15 06:58:35 +0200

    Update 12-exercises.js

--- src/js/12-exercises.js
+++ src/js/12-exercises.js
@@ -58,20 +58,7 @@ function renderExercises(data) {
                 <p class="card__text-info"><span>Target:</span>${target}</p>
               </div>
         </div>
-      </li>
-
-    .map(({ _id, name, burnedCalories, bodyPart, target }) => {
-      return `
-  <li class="filters__item">
-    <p class="filters__pre-title-second"></p>
-    <button class="filters__btn-second" data-id="${_id}" type="button">svg</button>
-    <h2 class="filters__title-second">${name}</h2>
-    <p class="filters__text-second"><span>Burned calories:</span>${burnedCalories}</p>
-    <p class="filters__text-second"><span>Body part:</span>${bodyPart}</p>
-    <p class="filters__text-second"><span>Target:</span>${target}</p>
-  </li>
-
-    `;
+      </li>`;
     })
     .join('');
 
