3a41d88c285afbf167106c880bb7116ee890e246
ViktorSvertoka <victor.svertoka@gmail.com>
2023-12-16 20:56:22 +0000

    Deploying to gh-pages from @ ViktorSvertoka/your-energy@9e82297619140fb47e7b3208ec6582510a9d964e 🚀

--- assets/favorites-c9c8c5e6.js
+++ assets/favorites-c9c8c5e6.js
@@ -1,4 +1,4 @@
-import{i as s}from"./05-modal-exercises-95b2d911.js";const d=document.querySelector(".favorites__list"),l=document.getElementById("favoritesButton");l.addEventListener("click",v());function v(){const t=JSON.parse(localStorage.getItem("exerciseData"));if(t.length!==0){d.innerHTML="";const a=t.map(({_id:e,name:r,burnedCalories:c,bodyPart:i,target:o})=>`
+import{i as s}from"./05-modal-exercises-1e83497c.js";const d=document.querySelector(".favorites__list"),l=document.getElementById("favoritesButton");l.addEventListener("click",v());function v(){const t=JSON.parse(localStorage.getItem("exerciseData"));if(t.length!==0){d.innerHTML="";const a=t.map(({_id:e,name:r,burnedCalories:c,bodyPart:i,target:o})=>`
       <li class="fav-filters__item-card">
         <div class="fav-card__wrap">
           <div class="fav-card__block-btn">
