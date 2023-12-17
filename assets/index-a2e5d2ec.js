92f6a955146949ad0770d72ccb75e356348674ed
ViktorSvertoka <victor.svertoka@gmail.com>
2023-12-17 18:36:07 +0000

    Deploying to gh-pages from @ ViktorSvertoka/your-energy@05e54507f6de39d41d6c7b801b1ea071e906dbbf 🚀

--- assets/index-a2e5d2ec.js
+++ assets/index-a2e5d2ec.js
@@ -1,4 +1,4 @@
-import{A as U,a as de,i as N}from"./05-modal-exercises-96cc9cd3.js";const ue=new U,X=document.querySelector(".js-list");V("Muscles");async function V(a){try{const u=await ue.getFilter(a);fe(u)}catch(u){console.log(u)}}function fe(a){const u=a.map(({filter:p,name:f,imgURL:n})=>`
+import{A as U,a as de,i as N}from"./05-modal-exercises-b8eabf1f.js";const ue=new U,X=document.querySelector(".js-list");V("Muscles");async function V(a){try{const u=await ue.getFilter(a);fe(u)}catch(u){console.log(u)}}function fe(a){const u=a.map(({filter:p,name:f,imgURL:n})=>`
   <li class="filters__item">
     <img class="filters__img-first" src="${n}"></img>
     <div class="filters__wrapper-first">
