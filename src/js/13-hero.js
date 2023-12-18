474befd34d3c9d531e9205d4251c00e15da631eb
Tetiana Zorii <tanyusha.zoriy@gmail.com>
2023-12-17 22:36:45 -0500

    Update bmi-modal

--- src/js/13-hero.js
+++ src/js/13-hero.js
@@ -20,27 +20,27 @@ svg.addEventListener('mouseout', function () {
   currentColorIndex = (currentColorIndex + 1) % colors.length;
 });
 
-const btnEl = document.getElementById('btn');
-const bmiInputEl = document.getElementById('bmi-result');
-const weightConditionEl = document.getElementById('weight-condition');
+// const btnEl = document.getElementById('btn');
+// const bmiInputEl = document.getElementById('bmi-result');
+// const weightConditionEl = document.getElementById('weight-condition');
 
-function calculateBMI() {
-  const heightValue = document.getElementById('height').value / 100;
-  const weightValue = document.getElementById('weight').value;
+// function calculateBMI() {
+//   const heightValue = document.getElementById('height').value / 100;
+//   const weightValue = document.getElementById('weight').value;
 
-  const bmiValue = (weightValue / (heightValue * heightValue)).toFixed(1);
+//   const bmiValue = (weightValue / (heightValue * heightValue)).toFixed(1);
 
-  bmiInputEl.value = bmiValue;
+//   bmiInputEl.value = bmiValue;
 
-  if (bmiValue < 18.5) {
-    weightConditionEl.innerText = 'Under weight!';
-  } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
-    weightConditionEl.innerText = 'Normal weight!';
-  } else if (bmiValue >= 25 && bmiValue <= 29.9) {
-    weightConditionEl.innerText = 'Overweight!';
-  } else if (bmiValue >= 30) {
-    weightConditionEl.innerText = 'Obesity!';
-  }
-}
+//   if (bmiValue < 18.5) {
+//     weightConditionEl.innerText = 'Under weight!';
+//   } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+//     weightConditionEl.innerText = 'Normal weight!';
+//   } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+//     weightConditionEl.innerText = 'Overweight!';
+//   } else if (bmiValue >= 30) {
+//     weightConditionEl.innerText = 'Obesity!';
+//   }
+// }
 
-btnEl.addEventListener('click', calculateBMI);
+// btnEl.addEventListener('click', calculateBMI);
