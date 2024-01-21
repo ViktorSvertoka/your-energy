e9e6a4b53af939a9c4307dc08a6012361d74674d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-01-21 19:15:48 +0200

    Update 08-scroll-up.js

--- src/js/08-scroll-up.js
+++ src/js/08-scroll-up.js
@@ -4,18 +4,14 @@ goTopBtn.addEventListener('click', goTop);
 window.addEventListener('scroll', trackScroll);
 
 function trackScroll() {
-  const offset = window.pageYOffset;
+  const offset = window.scrollY;
   const coords = document.documentElement.clientHeight;
-  if (offset > coords) {
-    goTopBtn.classList.add('scroll__go-top--show');
-  } else {
-    goTopBtn.classList.remove('scroll__go-top--show');
-  }
+  goTopBtn.classList.toggle('scroll__go-top--show', offset > coords);
 }
 
 function goTop() {
-  if (window.pageYOffset > 0) {
+  if (window.scrollY > 0) {
     window.scrollBy(0, -75);
-    setTimeout(goTop, 0);
+    requestAnimationFrame(goTop);
   }
 }
